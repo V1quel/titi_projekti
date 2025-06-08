6ce5aa7192f40a481833a055795e3d013ec8453d
Mikael <mikaeloj.peltomaa@gmail.com>
2025-06-08 11:36:17 +0300

    some changes

--- titi_otanta.py
+++ titi_otanta.py
@@ -11,7 +11,7 @@ tunnusluvut = data[1]
 odotusarvot = []
 otoskeskihajonnat = []
 
-for i in range(0,100):
+for i in range(0,1000):
     otos = np.random.choice(list, 10, True)
     odotusarvot.append(float(round(np.average(otos),2)))
     otoskeskihajonnat.append(float(round(np.std(otos, ddof=1),2)))
@@ -22,16 +22,23 @@ sigma_ka = np.mean(otoskeskihajonnat)
 mu0_label = f"Koko aineiston odotusarvo {round(tunnusluvut[3],1)}"
 sigma0_label = f"Koko aineiston keskihajonta {round(tunnusluvut[4],1)}"
 
+mu_CI = np.percentile(odotusarvot, [2.5, 97.5])
+sigma_CI = np.percentile(otoskeskihajonnat, [2.5, 97.5])
+
 plt.subplot(1,2,1)
 plt.hist(odotusarvot, bins=20, color='royalblue', edgecolor='black')
 plt.title("Odotusarvojen jakauma")
 plt.xlabel(mu0_label)
 plt.axvline(mu_ka, color='red')
+plt.axvline(mu_CI[0], color='yellow')
+plt.axvline(mu_CI[1], color='yellow')
 
 plt.subplot(1,2,2)
 plt.hist(otoskeskihajonnat, bins=30, color='darkorange', edgecolor='black')
 plt.title("Otoskeskihajontojen jakauma")
 plt.xlabel(sigma0_label)
 plt.axvline(sigma_ka, color='red')
+plt.axvline(sigma_CI[0], color='yellow')
+plt.axvline(sigma_CI[1], color='yellow')
 
 plt.show()
